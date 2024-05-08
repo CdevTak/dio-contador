b69728a8567a8488c3b74330fa4ffffe63eb381b
FabianaMP <fmarquespenteado@gmail.com>
2024-05-08 11:03:39 -0300

    Controle de Fluxo

---
+++ src/diojava/Contador.java
@@ -0,0 +1,45 @@
+package diojava;
+
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		int parametroUm;
+		int parametroDois;
+		
+		System.out.println("Digite o primeiro parâmetro");
+		parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if(parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException("O primeiro parâmetro não pode ser maior que o segundo");
+		}	
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+		for(int i = 0; i <= contagem; i++) {
+			System.out.println(parametroUm + i);
+		}
+	}
+}
+
+class ParametrosInvalidosException extends Exception {
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
+
